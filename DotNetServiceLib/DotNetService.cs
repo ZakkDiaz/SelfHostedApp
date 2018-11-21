ae95f76a246dd82322a841baeec0bd8742d062e9
Zakkery Diaz <zakkdiaz@hotmail.com>
2018-11-20 22:11:44 -0700

    Initial add

---
+++ DotNetServiceLib/DotNetService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetServiceLib
+{
+    public class DotNetService : IWebHost
+    {
+        public IFeatureCollection ServerFeatures { get; set; } = new FeatureCollection();
+
+        public IServiceProvider Services { get; set; } = ServiceProviderFactory();
+
+        public DotNetService(IClassInstance classInstance)
+        {
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Stopping");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting");
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            for(var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+                System.Threading.Thread.Sleep(1000);
+            }
+            return null;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            Console.WriteLine("Stopping");
+            System.Threading.Thread.Sleep(1000);
+            return null;
+        }
+
+
+        private static IServiceProvider ServiceProviderFactory()
+        {
+            HostingEnvironment env = new HostingEnvironment();
+            env.ContentRootPath = Directory.GetCurrentDirectory();
+            env.EnvironmentName = "Development";
+
+            Startup startup = new Startup(env);
+            ServiceCollection sc = new ServiceCollection();
+            startup.ConfigureServices(sc);
+            return sc.BuildServiceProvider();
+        }
+    }
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+        }
+
+        internal void ConfigureServices(IServiceCollection sc)
+        {
+            sc.AddSingleton<IClassInstance, MyInstance>();
+            sc.AddSingleton(sp => new WebHostOptions());
+        }
+    }
+
+    public class MyInstance : IClassInstance
+    {
+
+    }
+
+    public interface IClassInstance
+    {
+    }
+}
---
+++ SelfHostedApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using Autofac;
+using DotNetServiceLib;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SelfHostedApp
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");// Create your builder.
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DotNetService>().As<IWebHost>();
+            builder.RegisterType<MyInstance>().As<IClassInstance>();
+            Container = builder.Build();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var service = scope.Resolve<IWebHost>();
+                var serviceTask = service.StartAsync();
+                while (serviceTask != null && serviceTask.Status == TaskStatus.Running)
+                {
+                    Console.WriteLine("Waiting...");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
+    //public class Startup
+    //{
+    //    private readonly IHostingEnvironment _env;
+    //    private readonly IConfiguration _config;
+    //    private readonly ILoggerFactory _loggerFactory;
+
+    //    public Startup(IHostingEnvironment env, IConfiguration config,
+    //        ILoggerFactory loggerFactory)
+    //    {
+    //        _env = env;
+    //        _config = config;
+    //        _loggerFactory = loggerFactory;
+    //    }
+
+    //    public void ConfigureServices(IServiceCollection services)
+    //    {
+    //        var logger = _loggerFactory.CreateLogger<Startup>();
+
+    //        if (_env.IsDevelopment())
+    //        {
+    //            // Development service configuration
+
+    //            logger.LogInformation("Development environment");
+    //        }
+    //        else
+    //        {
+    //            // Non-development service configuration
+
+    //            logger.LogInformation($"Environment: {_env.EnvironmentName}");
+    //        }
+
+    //        // Configuration is available during startup.
+    //        // Examples:
+    //        //   _config["key"]
+    //        //   _config["subsection:suboption1"]
+    //    }
+    //}
+
+
